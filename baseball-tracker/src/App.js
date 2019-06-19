cc9dc81edf5c8d9b8cb651263af3eba8b701d4b6
Vijay Das <vijaytdas@gmail.com>
2019-06-19 14:53:18 -0500

    add onStrike function in App.js

--- baseball-tracker/src/App.js
+++ baseball-tracker/src/App.js
@@ -9,6 +9,28 @@ class App extends React.Component {
     hits: 0
   }
 
+  // add functions for baseball batter rules
+  // balls and strikes reset to 0 when a player reaches 3 strikes or 4 balls.
+  // balls and strikes reset to 0 when a hit is recorded.
+  // a foul increases strikes up to 2. With no strikes, a foul makes it 1 strike.
+  // With 1 strike, a foul makes it 2 strikes.
+  // With two strikes a foul has no effect, count stays at 2 strikes.
+  
+  onStrike = () => {
+    if (this.state.strikes + 1 > 2) {
+      this.setState({ strikes: 0 })
+    } else {
+      this.setState({ strikes: this.state.strikes + 1 });
+    }
+  }
+
+  
+
+
+//  onBall
+//  onFoul
+//  onHit
+
 
 
 render() {
